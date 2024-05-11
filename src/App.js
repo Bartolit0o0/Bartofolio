2f0061767804a348cb40c3479db095a3cef66a9e
Barto404 <131315049+Bartolit0o0@users.noreply.github.com>
2024-05-10 22:25:58 -0300

    zzz

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import React from 'react';
 function App() {
   return (
     <div className="background">
+    <div className="button-container">
+      <button className="button1">Button 1</button>
+      <button className="button1">Button 2</button>
+      <button className="button1">Button 3</button>
+    </div>
       <div className="container">
     </div>
     </div>
